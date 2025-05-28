7c507801ea127b5fdb20cc6cbadbd7dff9ced599
Natjimor <jimenezorozconatalia@gmail.com>
2025-05-28 12:36:11 -0500

    cambios en la landign

--- src/components/CallSection/CallSection.jsx
+++ src/components/CallSection/CallSection.jsx
@@ -3,12 +3,21 @@ import '../../styles/CallSection.css'
 export default function CalltoAction() {
 
   return (
-    <section className='CalltoActSect'>
+    <section className='CalltoActSect'
+    style={{
+                backgroundImage: `linear-gradient(to top, rgba(20, 20, 23, 0.85), rgba(20, 20, 23, 0.36)), url("https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2022/12/amigos-discutiendo-2901462.jpg?tf=3840x")`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+    }}
+    >
         <div className='CalltoActInfo'>
           <h2>¿Tu grupo de amigos nunca se decide a qué jugar? ¡Dejen que PlayMatch lo resuelva por ustedes!</h2>
           <p>Reúnanse, compartan sus gustos y reciban recomendaciones personalizadas en segundos.</p>
-          <button>Comienza desde la web</button>
-          <button>Comienza con nuestro Bot</button>
+          <div className='CalltoActBtn'>
+            <button>Comienza desde la web</button>
+            <button>Comienza con nuestro Bot</button>
+          </div>
         </div>
     </section>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className='PlaymatchNav'>
         <div className='HomeLogo'>
-            <img className='NavLogo' src="https://kbkljkxypthbtafchvxz.supabase.co/storage/v1/object/sign/assets/LogoPM.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5Xzc2MmJjMmFkLTJkMDYtNDljOC05MmQ2LTNlZjUzNWVlMDAzOCJ9.eyJ1cmwiOiJhc3NldHMvTG9nb1BNLnBuZyIsImlhdCI6MTc0Nzg4MjQwOCwiZXhwIjoxNzc5NDE4NDA4fQ.c2HKB4sFVheq-nyUBgPzQFTH7xSA4f5Oebye7YKzK4s" alt="playmatch" />
+            <img className='NavLogo' src="https://kbkljkxypthbtafchvxz.supabase.co/storage/v1/object/sign/assets/LogoPM.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5Xzc2MmJjMmFkLTJkMDYtNDljOC05MmQ2LTNlZjUzNWVlMDAzOCJ9.eyJ1cmwiOiJhc3NldHMvTG9nb1BNLnBuZyIsImlhdCI6MTc0NzkzMTIxNiwiZXhwIjoxNzc5NDY3MjE2fQ.HKtOrkOtEf3MQT_T22ggJF7gLe4GnnJbzrZsY9DfNiQ" alt="playmatch" />
             <p>Playmatch</p>
         </div>
         <div className='HomeButtons'>
